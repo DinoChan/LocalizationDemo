a0c7a6a82c66a1b2df98c9d2a9a950120645c37d
Dino Chan <dino.chan@live.com>
2017-10-19 16:39:05 +0800

    Finish LocalizationDemoWpf.

--- LocalizationDemoWpf/LocalizationDemoWpf/MainWindow.xaml.cs
+++ LocalizationDemoWpf/LocalizationDemoWpf/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,23 @@ namespace LocalizationDemoWpf
         public MainWindow()
         {
             InitializeComponent();
+            LanguageComboBox.SelectionChanged += OnSelectedLanguageChanged;
+        }
+
+        private void OnSelectedLanguageChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var culture = LanguageComboBox.SelectedIndex == 0 ? "zh-cn" : "en-us";
+            var cultureInfo = new System.Globalization.CultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+
+
+            ResourceDictionary dict = new ResourceDictionary { Source = new Uri($@"Resources\{culture}.xaml", UriKind.RelativeOrAbsolute) };
+            Application.Current.Resources.MergedDictionaries[0] = dict;
+
+            var message = TryFindResource("SwitchLanguage") as string;
+            if (string.IsNullOrWhiteSpace(message) == false)
+                MessageBox.Show(message);
         }
     }
 }
