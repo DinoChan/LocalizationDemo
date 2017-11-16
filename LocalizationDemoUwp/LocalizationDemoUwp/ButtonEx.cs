c61d9ba6aff6864f3d3145e7bfaaf93e6eca5ecf
Dino Chan <dino.chan@live.com>
2017-11-16 17:02:47 +0800

    ...

---
+++ LocalizationDemoUwp/LocalizationDemoUwp/ButtonEx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LocalizationDemoUwp
+{
+    public class ButtonEx
+    {
+        /// <summary>
+        //  从指定元素获取 Content 依赖项属性的值。
+        /// </summary>
+        /// <param name="obj">The element from which the property value is read.</param>
+        /// <returns>Content 依赖项属性的值</returns>
+        public static object GetContent(DependencyObject obj)
+        {
+            return (object)obj.GetValue(ContentProperty);
+        }
+
+        /// <summary>
+        /// 将 Content 依赖项属性的值设置为指定元素。
+        /// </summary>
+        /// <param name="obj">The element on which to set the property value.</param>
+        /// <param name="value">The property value to set.</param>
+        public static void SetContent(DependencyObject obj, object value)
+        {
+            obj.SetValue(ContentProperty, value);
+        }
+
+        /// <summary>
+        /// 标识 Content 依赖项属性。
+        /// </summary>
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.RegisterAttached("Content", typeof(object), typeof(ButtonEx), new PropertyMetadata(null, OnContentChanged));
+
+
+        private static void OnContentChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            Button target = obj as Button;
+            if (target == null)
+                return;
+
+            object oldValue = (object)args.OldValue;
+            object newValue = (object)args.NewValue;
+            if (oldValue == newValue)
+                return;
+
+            target.Content = newValue;
+        }
+
+
+
+
+    }
+}
---
+++ localizationdemouwp/dynamiclocalizationdemouwp/dynamicresources.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.Resources.Core;
+using DynamicLocalizationDemoUwp.Annotations;
+using Windows.UI.Xaml;
+
+namespace DynamicLocalizationDemoUwp
+{
+    public class DynamicResources : INotifyPropertyChanged
+    {
+        public DynamicResources()
+        {
+            _defaultContextForCurrentView = ResourceContext.GetForCurrentView();
+
+            _defaultContextForCurrentView.QualifierValues.MapChanged += async (s, m) =>
+            {
+                await MainPage.Current.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    OnPropertyChanged("");
+                });
+            };
+        }
+
+        private ResourceContext _defaultContextForCurrentView;
+
+        public string Main
+        {
+            get { return ResourceManager.Current.MainResourceMap.GetValue("DynamicResources/Main", _defaultContextForCurrentView).ValueAsString; }
+        }
+
+        public string Settings
+        {
+
+            get { return ResourceManager.Current.MainResourceMap.GetValue("DynamicResources/Settings", _defaultContextForCurrentView).ValueAsString; }
+        }
+
+        public string RestartNote
+        {
+            get { return ResourceManager.Current.MainResourceMap.GetValue("DynamicResources/RestartNote", _defaultContextForCurrentView).ValueAsString; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
